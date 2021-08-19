aa6f9c3938294091eed193c30e034601bcac3b4d
alvink28 <58781559+alvink28@users.noreply.github.com>
2021-08-19 16:55:52 +0800

    PBR

---
+++ Project_Template/scenebasic_uniform.h
@@ -0,0 +1,44 @@
+#ifndef SCENEBASIC_UNIFORM_H
+#define SCENEBASIC_UNIFORM_H
+
+#include "helper/scene.h"
+
+#include <glad/glad.h>
+#include "helper/glslprogram.h"
+#include "helper/random.h"
+#include "helper/grid.h"
+#include "helper/plane.h"
+#include "helper/objmesh.h"
+#include "helper/teapot.h"
+
+#include <glm/glm.hpp>
+
+class SceneBasic_Uniform : public Scene
+{
+private:
+
+    GLSLProgram prog;
+
+    Plane plane;
+    std::unique_ptr<ObjMesh> mesh;
+    Teapot teapot;
+
+    float tPrev, lightAngle, lightRotationSpeed;
+    glm::vec4 lightPos;
+
+    void setMatrices();
+    void compile();
+    void drawScene();
+    void drawFloor();
+    void drawSpot(const glm::vec3& pos, float rough, int metal, const glm::vec3& color);
+
+public:
+    SceneBasic_Uniform();
+
+    void initScene();
+    void update( float t );
+    void render();
+    void resize(int, int);
+};
+
+#endif // SCENEBASIC_UNIFORM_H
---
+++ Project_Template/shader/flat_frag.glsl
@@ -0,0 +1,10 @@
+#version 460
+
+uniform vec4 Color;
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = Color;
+}
